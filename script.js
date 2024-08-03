568a0efbfd0fa1c10908c8a20be31acb3f58b237
GAYATHIRI N <160384136+Gayathiri94@users.noreply.github.com>
2024-08-03 19:11:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    showSection('home'); // Show the home section by default when the page loads
-});
 
-function showSection(sectionId) {
-    // Hide all sections
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => section.style.display = 'none');
-
-    // Show the selected section
-    document.getElementById(sectionId).style.display = 'block';
-}
